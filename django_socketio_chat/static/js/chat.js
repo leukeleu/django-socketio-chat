3c97da2b67ef8bca0e3546707e206daeaef762c5
Marko Tibold <mtibold@leukeleu.nl>
2012-06-12 16:49:18 +0200

    serious refactoring and sorting out what belongs in user code and what belongs in the app.

---
+++ django_socketio_chat/static/js/chat.js
@@ -0,0 +1,152 @@
+var conn = null;
+var online_users = {};
+var user = 'anonymous';
+
+Chat = {
+
+  log:function(msg) {
+    var control = $('#log');
+    date = new Date();
+    timestamp =  date.getHours() + ':' +
+                 date.getMinutes()+ ':' +
+                 date.getSeconds();
+    control.html(control.html() + timestamp+ ': ' + msg + '<br/>');
+    control.scrollTop(control.scrollTop() + 1000);
+  },
+
+  connect: function() {
+    //disconnect();
+
+    // Socket.IO magic, $.map does not work and crashes socket.io.
+
+    // Hack to work around bug 251, I really hope it is going to be fixed.
+    // https://github.com/LearnBoost/socket.io-client/issues/251
+    // Alternative way to do full reconnect is to pass 'force new connection',
+    // but you will lose multiplexing.
+    io.j = [];
+    io.sockets = [];
+
+    conn = io.connect('https://' + window.location.host + '/chat', {
+              'force new connection': true,
+              //transports: transports,
+              rememberTransport: false,
+              resource: 'chat/socket.io'
+           });
+
+    this.log('Connecting...');
+    
+    var self = this;
+
+    conn.on('connect', function() {
+      self.log('Connected.');
+      self.update_ui();
+    });
+
+    conn.on('public_message', function(sender, message) {
+      self.log(sender + ' says: ' + message);
+      self.update_ui();
+    });
+
+    conn.on('private_message', function(sender, message) {
+      self.log('Private: ' + sender + ' says: ' + message);
+      self.update_ui();
+    });
+
+    conn.on('users', function(data){
+      online_users = data;
+      self.update_ui();
+    });
+    
+    conn.on('welcome', function(user_name){
+      self.log('Received welcome from server.');
+      user = user_name;
+      self.update_ui();
+    });
+
+    conn.on('user_joined', function(user){
+      self.log(user + ' joined the chat.');
+      online_users[user] = true;
+      self.update_ui();
+    });
+
+    conn.on('user_left', function(user){
+      self.log(user + ' left the chat.');
+      online_users[user] = false;
+      self.update_ui();
+    });
+
+    conn.on('disconnect', function(data) {
+      self.log('Disconnected.');
+      conn = null;
+      self.update_ui();
+    });
+  },
+
+  disconnect: function() {
+    if (conn !== null) {
+      this.log('Disconnecting...');
+
+      conn.disconnect();
+      conn = null;
+
+      this.update_ui();
+    }
+  },
+
+  update_ui: function() {
+    var msg = '';
+
+    if (conn == null || !conn.socket || !conn.socket.connected) {
+      msg = 'disconnected';
+      $('#connect').text('Connect');
+    } else {
+      msg = 'connected (' + conn.socket.transport.name + ') as ' + '<b>' + user + '</b>';
+      $('#connect').text('Disconnect');
+    }
+
+    $('#status').html(msg);
+
+    users = $('#participants');
+    users.html('');
+    $.each(online_users, function(key, val) {
+      var onl = 'offline';
+      if (val) {
+       onl = '<b>online</b>';
+      }
+      users.html(users.html() + '<input type="radio" name="participant" val="' + key+ '"/> ' + key + ' ' + onl + '</br>');
+    });
+  },
+
+  init: function() {
+    var self= this;
+    $('#connect').click(function() {
+      if (conn === null) {
+        self.connect();
+      } else {
+        self.disconnect();
+      }
+      self.update_ui();
+      return false;
+    });
+
+    $('form.#chatform').submit(function() {
+      var text = $('#text').val();
+      self.log('Sending: ' + text);
+      conn.emit('public_message', text);
+      $('#text').val('').focus();
+      return false;
+    });
+
+    $('form.#private-chatform').submit(function() {
+      var text = $('#private-text').val();
+      
+      target_user = $('#private-chatform input:checked')[0].getAttribute('val');
+      if (target_user) {
+        self.log('Sending private message: ' + text + ' to ' + target_user);
+        conn.emit('private_message', target_user, text);
+      }
+      $('#text').val('').focus();
+      return false;
+    });
+  }
+};
