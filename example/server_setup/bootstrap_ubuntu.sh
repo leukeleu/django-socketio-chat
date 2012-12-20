29221296999ee1fb14436d178cee7b6a138a7617
Marko Tibold <marko@tibold.nl>
2012-12-20 13:09:20 +0100

    Document bootstrap script some more.

--- example/server_setup/bootstrap_ubuntu.sh
+++ example/server_setup/bootstrap_ubuntu.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+# Ask for sudo password upfront
 sudo -v
 
-echo "Installing haproxy 1.5"
 # Normal install
+echo "Installing haproxy 1.5"
 sudo apt-get install haproxy
 
 # Override normal install with custom build of 1.5
@@ -13,13 +14,12 @@ sudo apt-get build-dep haproxy
 make TARGET=linux2628 USE_STATIC_PCRE=1 USE_OPENSSL=1
 sudo make install
 
-# clean up
-cd ..
-rm -r haproxy-ss-20121204 && rm haproxy-ss-20121204.tar.gz 
+# clean up temp fiels and dirs
+cd .. && rm -r haproxy-ss-20121204 && rm haproxy-ss-20121204.tar.gz 
 
-# Make sure haproxy is enabled
-sudo  sed "s/^ENABLED.*$/ENABLED=1/" -i /etc/default/haproxy 
+# Make sure haproxy is enabled by replacing line in config file
+sudo sed "s/^ENABLED.*$/ENABLED=1/" -i /etc/default/haproxy 
 
-# point init.d to our binary
+# point init.d to our binary by replacing line in config file
 sudo cp /etc/init.d/haproxy /etc/init.d/haproxy.orig
 sudo sed "s/^HAPROXY.*$/HAPROXY=\/usr\/local\/sbin\/haproxy/" -i /etc/default/haproxy 
