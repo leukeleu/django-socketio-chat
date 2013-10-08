3046ce8ecc8e46a2eddc9354652494922339cfa8
Dennis Bunskoek <dbunskoek@leukeleu.nl>
2013-10-08 17:59:49 +0200

    Removed debugging functionality

--- django_socketio_chat/templatetags/django_socketio_chat_tags.py
+++
@@ -1,9 +0,0 @@
-from django.conf import settings
-from django import template
-
-register = template.Library()
-
-@register.simple_tag(takes_context=True)
-def django_socketio_chat_is_debug(context):
-    context['debug'] = settings.DEBUG
-    return ''
